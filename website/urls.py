d7f8dceb1f89e03c1d7b3b7662145dd57a601d92
Aleksi Könönen <aleksi.kononen@student.tut.fi>
2019-02-21 10:48:47 +0200

    urls changed

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("home/", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
-    #path("login/", views.login, name="login"),
     path("profile/", views.profile, name="profile"),
     path("profile/remove_rating/<int:movie_rating_id>", views.remove_rating, name="remove_rating"),
     path("profile/change_rating/<int:movie_rating_id>/<int:new_rating>", views.change_rating, name="change_rating")
